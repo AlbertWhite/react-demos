c3702f66de36091cb594e8d97d7bf191889418d0
Albert XU <xyb1114@gmail.com>
2020-04-08 17:48:37 +0200

    fixup

--- demo44-use-exterior-lib/src/App.js
+++ demo44-use-exterior-lib/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Text, Link } from 'exterior-ui-lib';
+import { Text, Link, Box } from 'exterior-ui-lib';
 import './App.css';
 
 function App() {
   console.warn('ax', Text);
   return (
     <div className='App'>
-      <Text>Hello world</Text>
-      <Link href='https://www.google.com'>Hello world</Link>
+      <Box marginTop={'180px'}>
+        <Text>Hello world</Text>
+        <Link href='https://www.google.com'>Hello world</Link>
+      </Box>
     </div>
   );
 }
