682d9eb28586c55c3ca5275c29f5052cfce42afd
yuebaiXu <xyb1114@gmail.com>
2020-12-12 18:07:50 +0100

    add new demo

---
+++ demo49-compound-component/src/App.js
@@ -0,0 +1,14 @@
+import SubApp from './Sub-App'
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      hello
+    </div>
+  );
+}
+
+App.SubComponent = SubApp
+
+export default App;
